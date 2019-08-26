322347aae524f1dc1299b2c76e99bcb5bd7c9e61
Aron Høyer <aron@hoyer.com>
2019-08-26 11:35:06 +0200

    Add input validation

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ function sanitizeForUri(id) {
 }
 
 module.exports = function generateBase64ID(len, prepend) {
+  if (len && typeof len !== 'number') {
+    const err = new Error(`Invalid length argument. Expected number, but got ${typeof len}`);
+    err.name = 'ERR_INVALID_ARGS';
+    throw err;
+  }
+
+  if (prepend && typeof prepend !== 'string') {
+    const err = new Error(`Invalid prepend argument. Expected number, but got ${typeof prepend}`);
+    err.name = 'ERR_INVALID_ARGS';
+    throw err;
+  }
+
   let id = sanitizeForUri(generate(len || 10));
 
   if (parseFloat(len) % 2 !== 0 && id.length < len) {
--- tests/pertyid.test.js
+++ tests/pertyid.test.js
@@ -1,23 +1,45 @@
 const { expect } = require('chai');
 
-const b64id = require('../index');
+const pertyid = require('../index');
 
-describe('b64id', () => {
+describe('pertyid', () => {
   it('should generate a base64 string', () => {
     const regex = new RegExp('[a-zA-Z0-9_-]', 'g');
-    expect(regex.test(b64id())).to.equal(true);
+    expect(regex.test(pertyid())).to.equal(true);
   });
 
   it('should generate a base64 string of a default length', () => {
-    expect(b64id().length).to.equal(10);
+    expect(pertyid().length).to.equal(10);
   });
 
   it('should generate a base64 string of a given length', () => {
-    [15, 32, 6, 11, 87, 17, 1, 5, 3].forEach((len) => expect(b64id(len).length).to.equal(len));
+    [15, 32, 6, 11, 87, 17, 1, 5, 3].forEach((len) => expect(pertyid(len).length).to.equal(len));
   });
 
   it('should prepend the base64 string with a given string', () => {
     const pre = 'pre-';
-    expect(b64id(10, pre)).to.contain(pre);
+    expect(pertyid(10, pre)).to.contain(pre);
+  });
+
+  it('should not generate a base64 string with invalid length argument', () => {
+    try {
+      expect(pertyid('not a number'));
+    } catch (err) {
+      expect(err.name).to.equal('ERR_INVALID_ARGS');
+      return true;
+    }
+
+    throw new Error('should not generate a base64 string with invalid length argument');
+  });
+
+  it('should not generate a base64 string with invalid prepend argument', () => {
+    try {
+      expect(pertyid(10, 666));
+    } catch (err) {
+      expect(err.name).to.equal('ERR_INVALID_ARGS');
+      return true;
+    }
+
+    throw new Error('should not generate a base64 string with invalid length argument');
   });
 });
