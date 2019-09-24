cba32cc7383ed012950d9ae039412ca48020d697
Aron Høyer <aron@hoyer.com>
2019-09-24 11:11:37 +0200

    Add and export defaultLength. Recursively generate id if prefix ends with dash or underscore. Support es6 import syntax

--- index.js
+++ index.js
@@ -8,26 +8,39 @@ function sanitizeForUri(id) {
   return id.replace(/\//g, '-').replace(/\+/g, '_').replace(/=/g, '');
 }
 
-function generateBase64ID(len, prepend) {
+const defaultLength = 10;
+
+function pertyid(len, prefix) {
   if (len && typeof len !== 'number') {
     const err = new Error(`Invalid length argument. Expected number, but got ${typeof len}`);
     err.name = 'ERR_INVALID_ARGS';
     throw err;
   }
 
-  if (prepend && typeof prepend !== 'string') {
-    const err = new Error(`Invalid prepend argument. Expected number, but got ${typeof prepend}`);
+  if (prefix && typeof prefix !== 'string') {
+    const err = new Error(`Invalid prefix argument. Expected string, but got ${typeof prefix}`);
     err.name = 'ERR_INVALID_ARGS';
     throw err;
   }
 
-  let id = sanitizeForUri(generate(len || 10));
+  let id = sanitizeForUri(generate(len || defaultLength));
+
+  while (
+    prefix
+    && (prefix.charAt(prefix.length - 1) === '-' || prefix.charAt(prefix.length - 1) === '_')
+    && (id.charAt(0) === '-' || id.charAt(0) === '_')
+  ) {
+    return pertyid(len, prefix);
+  }
 
   if (parseFloat(len) % 2 !== 0 && id.length < len) {
     id += sanitizeForUri(generate(2).charAt(0));
   }
 
-  return `${prepend || ''}${id}`;
+  return `${prefix || ''}${id}`;
 }
 
-module.exports = generateBase64ID;
+module.exports = pertyid;
+module.exports.default = pertyid;
+
+module.exports.defaultLength = defaultLength;
